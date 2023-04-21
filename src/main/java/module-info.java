dc19beffc53fc63b3e74697e5cd374c37a77513b
skylarrohter <srohter@waprep.org>
2023-04-21 10:05:44 -0700

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module net.novahc.smanagement {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens net.novahc.smanagement to javafx.fxml;
+    exports net.novahc.smanagement;
+}
