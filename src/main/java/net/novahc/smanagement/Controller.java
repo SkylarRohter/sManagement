72e6f4209c7a266d5d95f7b8c8d68fcf4cbd29d0
skylarrohter <skyriversub@gmail.com>
2023-11-02 14:46:22 -0700

    Added toggleable buttons

--- src/main/java/net/novahc/smanagement/Controller.java
+++ src/main/java/net/novahc/smanagement/Controller.java
@@ -9,9 +9,14 @@ import javafx.scene.layout.FlowPane;
 
 import java.lang.reflect.Array;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+    //FIELDS
+    private int currentlyEnabledIndex = 0;
+    private Button[] buttons;
+
     //MAIN PANES
     @FXML
     private AnchorPane mainPane;
@@ -40,10 +45,29 @@ public class Controller implements Initializable {
         bindButton(aButton);
         bindButton(umButton);
         bindButton(sButton);
+        buttons = new Button[]{sbButton, aButton, umButton, sButton};
+        for(Button button : buttons){
+            button.setOnAction(event -> handleButtonClick((Button) event.getSource()));
+        }
+
 
     }
     public void bindButton(Button b){
         b.prefWidthProperty().bind(sidePane.widthProperty());
         b.getStyleClass().add("sidebar-buttons");
+    }
+
+    private void handleButtonClick(Button clickedButton) {
+        // Enable the previously disabled button
+        buttons[currentlyEnabledIndex].setDisable(false);
+
+        // Find the index of the clicked button and disable it
+        for (int i = 0; i < buttons.length; i++) {
+            if (buttons[i] == clickedButton) {
+                currentlyEnabledIndex = i;
+                buttons[i].setDisable(true);
+                break;
+            }
+        }
     }
 }
---
+++ src/main/java/net/novahc/smanagement/functions/Toggler.java
@@ -0,0 +1,28 @@
+package net.novahc.smanagement.functions;
+
+import javafx.scene.control.Button;
+
+import java.util.ArrayList;
+
+public class Toggler {
+    private ArrayList<Button> buttons;
+    public Toggler(ArrayList<Button> buttons){
+        this.buttons = buttons;
+        init();
+    }
+    public void init(){
+        for(Button b : buttons){
+            b.setOnAction(event -> {
+                toggleButton(b);
+            });
+        }
+    }
+    public void toggleButton(Button button){
+        boolean toggled = button.isDisabled();
+        if(toggled){
+            button.setDisable(true);
+        } else {
+            button.setDisable(false);
+        }
+    }
+}
